9f2d1531e5b2f24cbfaf41a727a57a963ab59219
Sidney Burks <sid137@gmail.com>
2013-03-23 04:34:47 +0100

    adding missing file

---
+++ recipes/security.rb
@@ -0,0 +1,21 @@
+# http://www.cyberciti.biz/tips/linux-security.html
+# disable root login with passwords
+# disable telnet, ftp, rlogin, rsh
+# Password Restrictions
+#   password aging?
+#   password reuse
+# Lock accounts after Login Failures
+# no accounts with empty passwords
+# no uid0 accounts
+# Disable root login
+# Networking
+#   IPtables
+#   Check listening services
+#
+# Intrusion Detection System
+# Backups
+#   Server
+#   Database
+#   Logs
+#
+# http://www.linux-sxs.org/security/scheck.html
